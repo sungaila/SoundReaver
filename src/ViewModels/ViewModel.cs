7b818515d7f19fbd912a48edcc9ec35c2ccea227
David Sungaila <david.sungaila@arcor.de>
2026-01-19 12:34:17 +0100

    Update NuGet packages

--- src/ViewModels/ViewModel.cs
+++ src/ViewModels/ViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Sungaila.SoundReaver.ViewModels
 {
+    [RequiresUnreferencedCode("Inherits from ObservableValidator which uses RequiresUnreferencedCode.")]
     public partial class ViewModel : ObservableObject
     {
     }
